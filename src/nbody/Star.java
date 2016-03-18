acb1576711e6cbed6fb917a13e3e318fadb327ae
Conor Burrows <conor@ironicrobot.com>
2016-03-18 10:40:01 -0600

    provided equation for star shine

--- src/nbody/Star.java
+++ src/nbody/Star.java
@@ -9,13 +9,20 @@ package nbody;
  *
  * @author Conor
  */
+
+// cos equation for brightness between 65 and 255
+// y = 95cos(.1x) + 160
 public class Star {
     private double brightness; // brightness
     private final int minLight; 
     private final int maxLight;
     
     public Star() {
-        this.minLight = 127;
+        this.minLight = 65;
         this.maxLight = 255;
+    } // Star()
+    
+    public void shine() {
+        
     }
 }
